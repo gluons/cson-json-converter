681d2ebc1affb1f1db1bc5bb49a6732e781fc6c2
Saran Tanpituckpong <sarunta@gmail.com>
2018-04-03 21:14:03 +0700

    :building_construction: Change bundler from Parcel to Poi

---
+++ poi.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	entry: './src/main.js',
+	transformModules: ['coffeescript'],
+	html: {
+		title: 'CSON - JSON Converter',
+		description: 'An online CSON - JSON converter.'
+	},
+	minimize: false
+};
